9171f34e5b6e6d37cc7d5afb9f9d377b97c6d9c8
hamster666 <laura@digital-detox.co.uk>
2016-04-26 22:19:39 +0100

    Add markers to plot venues on map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,17 @@
  * Controller of the scoutApp
  */
  angular.module('scoutApp')
- 	.controller('MainCtrl', [ '$http', '$scope', function ($http, $scope) {
+ .controller('MainCtrl', [ '$http', '$scope', function ($http, $scope) {
 
  	angular.extend($scope, {
-        place: {
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 8
-        }
-    });
-
-    console.log($scope.place);
+ 		place: {
+ 			lat: 51.505,
+ 			lng: -0.09,
+ 			zoom: 9
+ 		},
+ 		markers: {}, 
+ 		data: []
+ 	});
 
  	var main = this;
  	var clientId = 'UNHXHYYH0QA5OERAA0WCQDRNWXHAUAFQXEHX4E10WL0LIO2V';
@@ -34,18 +34,26 @@
 
  		if(form.$valid){
 
-	 		$http.get(url).success(function(data){
+ 			$http.get(url).success(function(data){
 
-	 			main.data = data;
+ 				main.data = data;
 
-	 			
-	 			$scope.place.lat = data.response.geocode.feature.geometry.center.lat;
-	 			$scope.place.lng = data.response.geocode.feature.geometry.center.lng;
-	 			console.log($scope.place);
+ 				var coords = data.response.geocode.feature.geometry.center;
+ 				var venues = main.data.response.venues;
 
+ 				$scope.place.lat = coords.lat;
+ 				$scope.place.lng = coords.lng;
 
-	 		});
+ 				angular.forEach(venues, function(value, key) {
+
+ 					$scope.markers['venue'+key] = {};
+					$scope.markers['venue'+key].lat = value.location.lat;
+					$scope.markers['venue'+key].lng = value.location.lng;
+
+ 				});
+
+ 			});
  		}
  	};
 
-}]);
+ }]);
